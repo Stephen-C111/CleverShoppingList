5e61c7c9b8d4123642be27e6ccafe0fec836d3f2
Stephen <srcjemail665@gmail.com>
2021-04-04 15:45:05 -0500

    Restructure Reports page Add functionality: View most expensive items.

--- CleverShoppingList/ReportsPage.xaml.cs
+++ CleverShoppingList/ReportsPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using CleverShoppingList.Models;
+using CleverShoppingList.ViewModels;
 
 namespace CleverShoppingList
 {
@@ -15,6 +17,22 @@ namespace CleverShoppingList
         public ReportsPage()
         {
             InitializeComponent();
+        }
+
+        private async void ExpensiveItems_Clicked(object sender, EventArgs e)
+        {
+            await ReportsViewModel.rpvm.GetItemsByExpense();
+            ReportsViewModel.rpvm.ExpensiveItems = true;
+            ReportsViewModel.rpvm.MonthlySpending = false;
+            ReportsViewModel.rpvm.SelectOption = false;
+        }
+
+        private void Monthly_Spending_Clicked(object sender, EventArgs e)
+        {
+
+            ReportsViewModel.rpvm.ExpensiveItems = false;
+            ReportsViewModel.rpvm.MonthlySpending = true;
+            ReportsViewModel.rpvm.SelectOption = false;
         }
     }
 }
---
+++ CleverShoppingList/ViewModels/ReportsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using CleverShoppingList.Models;
+using CleverShoppingList.ViewModels;
+using System.Threading.Tasks;
+
+namespace CleverShoppingList.ViewModels
+{
+    class ReportsViewModel : ViewModelBase
+    {
+        public static ReportsViewModel rpvm;
+
+        bool selectOption = true;
+        bool monthlySpending;
+        bool expensiveItems;
+        ObservableCollection<Item> itemList = new ObservableCollection<Item>();
+
+        public bool SelectOption { get => selectOption; set { SetProperty(ref selectOption, value); } }
+        public bool MonthlySpending { get => monthlySpending; set { SetProperty(ref monthlySpending, value); } }
+        public bool ExpensiveItems { get => expensiveItems; set { SetProperty(ref expensiveItems, value); } }
+        public ObservableCollection<Item> ItemList { get => itemList; set { SetProperty(ref itemList, value); } }
+
+        public ReportsViewModel()
+        {
+            rpvm = this;
+        }
+
+        public void GetSpendingForThisMonth()
+        {
+
+        }
+
+        public void GetSpendingForAllMonths()
+        {
+
+        }
+
+        public async Task GetItemsByExpense()
+        {
+            ItemList = new ObservableCollection<Item>(await TabsViewModel.tvm.Conn.QueryAsync<Item>("SELECT ID, Name, Price, Purchased, TotalCost FROM Items ORDER BY TotalCost DESC"));
+        }
+    }
+}
